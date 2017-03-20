25bdf47212e083f12956d37ce88fa8c2c180abbd
Patrick Xie <Patrick.y.xie@gmail.com>
2017-03-20 12:06:50 -0700

    finished extrapolating routes to route handler

--- snazr-server/server/routeHandlers.js
+++ snazr-server/server/routeHandlers.js
@@ -1,3 +1,9 @@
+const { client } = require('./cache/redis.js')
+const User = require('./models/users');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/snazr');
+
 const token = {
     get: (req, res) => {
         res.send('hey thi sis get')
@@ -9,20 +15,87 @@ const token = {
 
 const toggle = {
     get: (req, res) => {
-
+        // should send a list of all the users that have their discoverability toggled on
+        // with x, y amount of range in terms of longitude & latitude
+        // by retrieving the information from our cache
+        // the information of location is sent through their locations
+        // if there are no locations just give a generic guy called joe back
+        let data = req.query 
+        //specify range to do search here, 0.02 means within 2 kilometers
+        client.util.get(data, client , 0.02) 
+        .then(result => res.send(result))
     },
     post: (req, res) => {
-
+        let user = req.body
+        client.util.add(user, client);
+        res.send('sent /api/toggled_users and adding')
     },
-    remove: (req, res) => {
-        
+    delete: (req, res) => {
+        let user = req.body
+        client.util.del(user, client);
+        res.send('deleting user from discovered list')
+    }
+}
+
+
+const photo = {
+    get: (req, res) => {
+        let { userId } = req.query 
+        let model = { userId }
+        User.find(model, function(err, result){
+            if (err) console.log('could not find user from database: ', model.userId)
+            console.log('from our database we got: ', result)
+            res.send(result)
+        })
+    },
+    post: (req, res) => {
+        let { userId, requestId, cloudStorageUrl } = req.body
+        let model = {
+            userId: requestId
+        }
+        User.find(model, function(err, result){
+            if(err) console.log('trying to save new photos, but cant find user: ', model.userId)
+            if(result.length > 0){
+            console.log('weve retrieved from db: ', result)
+            let toSave = result[0]
+            let photos = [...toSave.photos]
+            toSave.photos = [ cloudStorageUrl, ...photos ]
+
+            toSave.save()
+                .then(function(result){
+                console.log('1: saved photos successfully')
+                res.send('photo saved')
+                })
+                .catch(function(err){
+                console.log('ERRROR IS: ', err)
+                console.log('1: did not save photos successfully')
+                res.send('photo did not save, err!')
+                })
+            } else {
+                let photos = [ cloudStorageUrl ]
+                let toSave = new User({
+                userId: requestId,
+                photos: photos
+                })
+                toSave.save()
+                .then(function(result){
+                console.log('2: saved photos successfully')
+                res.send('photo saved')
+                })
+                .catch(function(err){
+                console.log('2: did not save photos successfully')
+                res.send('photo did not save, err!')
+                })
+            }
+            
+        })
     }
 }
 
 const handlers = {
     token,
-    toggle
-
+    toggle,
+    photo 
 }
 
 
--- snazr-server/server/server.js
+++ snazr-server/server/server.js
@@ -16,7 +16,7 @@ const Expo = require('exponent-server-sdk');
 const { handlers } = require('./routeHandlers.js')
 
 const port = process.env.PORT || 8000;
-mongoose.connect('mongodb://localhost/snazr');
+// mongoose.connect('mongodb://localhost/snazr');
 
 let expo = new Expo();
 
@@ -30,44 +30,41 @@ app.use('/bundles', express.static(path.join(__dirname, '/../bundles')));
 
 
 
-
-// app.post('/api/push_token', function(req, res){
-//   let data = req.body
-//   res.send('what is up mang')
-//   // if the user did not grant permission there would be no request at all
-
-// })
-
-
 app.route('/api/push_token')
   .get(handlers.token.get)
   .post(handlers.token.post)
 
+app.route('/api/toggled_users')
+  .get(handlers.toggle.get)
+  .post(handlers.toggle.post)
+  .delete(handlers.toggle.delete)
 
-app.get('/api/toggled_users', function(req, res){
-  //should send a list of all the users that have their discoverability toggled on
-  //with x, y amount of range in terms of longitude & latitude
-  //by retrieving the information from our cache
-  // the information of location is sent through their locations
-  // if there are no locations just give a generic guy called joe back
-  // 
-  
-  let data = req.query 
-  client.util.get(data, client , 0.02)  //specify range here, 0.02 means within 2kilometer
-  .then(result => res.send(result))
-})
+app.route('/api/photos')
+  .get(handlers.photo.get)
+  .post(handlers.photo.post)
 
-app.post('/api/toggled_users', function(req, res){
-  let user = req.body
-  client.util.add(user, client);
-  res.send('sent /api/toggled_users and adding')
-})
+server.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
 
-app.post('/api/toggle_off', function(req, res){
-  let user = req.body
-  client.util.del(user, client);
-  res.send('deleting user from discovered list')
-})
+
+// app.get('/api/toggled_users', function(req, res){
+//   let data = req.query 
+//   client.util.get(data, client , 0.02) 
+//   .then(result => res.send(result))
+// })
+
+// app.post('/api/toggled_users', function(req, res){
+//   let user = req.body
+//   client.util.add(user, client);
+//   res.send('sent /api/toggled_users and adding')
+// })
+
+// app.post('/api/toggle_off', function(req, res){
+//   let user = req.body
+//   client.util.del(user, client);
+//   res.send('deleting user from discovered list')
+// })
 
 
 // app.get('/test_socket', function(req, res){
@@ -86,32 +83,31 @@ app.post('/api/toggle_off', function(req, res){
 // })
 
 
-io.on('connection', function(socket){
-  console.log('a new user connected: ', socket.id)
-  socket.on('sending photo', function(msg){
-    console.log('we got message1: ', msg)
-    socket.emit('response', 'hello velix how are u')
-  })
+// io.on('connection', function(socket){
+//   console.log('a new user connected: ', socket.id)
+//   socket.on('sending photo', function(msg){
+//     console.log('we got message1: ', msg)
+//     socket.emit('response', 'hello velix how are u')
+//   })
 
-  socket.on('image', function(img){
-    let name, buffer = { img }
-    let myPath = path.join(__dirname, name)
-    fs.writeFile(myPath, img, 'binary',  function(err) {
-    if(err) {
-        return console.log(err);
-    }
+//   socket.on('image', function(img){
+//     let name, buffer = { img }
+//     let myPath = path.join(__dirname, name)
+//     fs.writeFile(myPath, img, 'binary',  function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
 
-    console.log("The file was saved!");
-    }); 
-  })
+//     console.log("The file was saved!");
+//     }); 
+//   })
 
-  socket.on('disconnect', function(){
-    console.log('weve disconnected')
-  })
+//   socket.on('disconnect', function(){
+//     console.log('weve disconnected')
+//   })
 
+// })
 
-
-})
 // io.on('connection', function(socket){
 
 //   socket.on('auth', function(userId){
@@ -143,64 +139,59 @@ io.on('connection', function(socket){
 //   });
 // });
       
-app.post('/photos', function(req, res){
-  let { userId, requestId, cloudStorageUrl } = req.body
-  let model = {
-    userId: requestId
-  }
-  User.find(model, function(err, result){
-    if(err) console.log('trying to save new photos, but cant find user: ', model.userId)
-    if(result.length > 0){
-      console.log('weve retrieved from db: ', result)
-      let toSave = result[0]
-      let photos = [...toSave.photos]
-      toSave.photos = [ cloudStorageUrl, ...photos ]
+// app.post('/photos', function(req, res){
+//   let { userId, requestId, cloudStorageUrl } = req.body
+//   let model = {
+//     userId: requestId
+//   }
+//   User.find(model, function(err, result){
+//     if(err) console.log('trying to save new photos, but cant find user: ', model.userId)
+//     if(result.length > 0){
+//       console.log('weve retrieved from db: ', result)
+//       let toSave = result[0]
+//       let photos = [...toSave.photos]
+//       toSave.photos = [ cloudStorageUrl, ...photos ]
 
-      toSave.save()
-        .then(function(result){
-          console.log('1: saved photos successfully')
-          res.send('photo saved')
-        })
-        .catch(function(err){
-          console.log('ERRROR IS: ', err)
-          console.log('1: did not save photos successfully')
-          res.send('photo did not save, err!')
-        })
-    } else {
-        let photos = [ cloudStorageUrl ]
-        let toSave = new User({
-          userId: requestId,
-          photos: photos
-        })
-        toSave.save()
-        .then(function(result){
-          console.log('2: saved photos successfully')
-          res.send('photo saved')
-        })
-        .catch(function(err){
-          console.log('2: did not save photos successfully')
-          res.send('photo did not save, err!')
-        })
-    }
+//       toSave.save()
+//         .then(function(result){
+//           console.log('1: saved photos successfully')
+//           res.send('photo saved')
+//         })
+//         .catch(function(err){
+//           console.log('ERRROR IS: ', err)
+//           console.log('1: did not save photos successfully')
+//           res.send('photo did not save, err!')
+//         })
+//     } else {
+//         let photos = [ cloudStorageUrl ]
+//         let toSave = new User({
+//           userId: requestId,
+//           photos: photos
+//         })
+//         toSave.save()
+//         .then(function(result){
+//           console.log('2: saved photos successfully')
+//           res.send('photo saved')
+//         })
+//         .catch(function(err){
+//           console.log('2: did not save photos successfully')
+//           res.send('photo did not save, err!')
+//         })
+//     }
     
-  })
-  //checks the database for the user see if the user exist,
-  // if user exist then append to the photos array
-  // otehrwise if the user does not exist
-  // create new user with a new array and the cloudStorageUrl be the first entry
-  // when user hits refresh on gallery page, it'll hit the get endpoint 
-})
+//   })
+// })
 
-app.get('/photos', function(req, res){
-  let { userId } = req.query 
-  let model = { userId }
-  User.find(model, function(err, result){
-    if (err) console.log('could not find user from database: ', model.userId)
-    console.log('from our database we got: ', result)
-    res.send(result)
-  })
+// app.get('/photos', function(req, res){
+//   let { userId } = req.query 
+//   let model = { userId }
+//   User.find(model, function(err, result){
+//     if (err) console.log('could not find user from database: ', model.userId)
+//     console.log('from our database we got: ', result)
+//     res.send(result)
+//   })
 
-})
+// })
 
 
 
@@ -222,7 +213,4 @@ app.get('/photos', function(req, res){
 
 
 
-server.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-});
 
